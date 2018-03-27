593c5d6d6bfd483ecf4ce61a2a9e524da4c0773a
Meric BAL <MB@Meric-MacBook-Air.local>
2018-03-26 19:03:27 -0700

    Updates

--- app/controllers/bobbleheads_controller.rb
+++ app/controllers/bobbleheads_controller.rb
@@ -6,5 +6,16 @@ class BobbleheadsController < ApplicationController
 		@sp = @bobbleheads.where(category: 'SPECIAL')
 	end
 
+	def find
+		@b = Bobblehead.find(params[:id])
+		if current_user.bobbleheads.include? @b
+			redirect_to bobbleheads_path, alert: 'Nooo'
+		else
+			@b.found = true
+			current_user.bobbleheads << @b
+			redirect_to bobbleheads_path, notice: 'Yess'
+		end
+	end
+
 	#
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
 	root to: "application#index"
 
 	get '/test1' => 'application#test1'
-	get '/bobbleheads' => 'bobbleheads#index'
+	# get '/bobbleheads' => 'bobbleheads#index'
+	# get '/find' => 'bobbleheads#find'
+
+	resources :bobbleheads, only: [:index] do
+		member { get 'find'}
+	end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
